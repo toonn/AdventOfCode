2dc5516c3a284cbb03123e20bbc58057c6ede453
toonn <toonn@toonn.io>
2020-12-09 14:52:42 +0100

    Solve day 9 part 2 in 1.8 ms

--- 2020/Day 9: Encoding Error/EncodingError.hs
+++ 2020/Day 9: Encoding Error/EncodingError.hs
@@ -65,27 +65,62 @@ firstNonSum nrs = foldr crease
       | valid summands nr = next (push nr summands)
       | otherwise = nr
 
-part1 :: Parsed Numbers -> IO ()
+part1 :: Parsed Numbers -> IO (Parsed Integer)
 part1 input = do
   let answer = firstNonSum <$> input
   printAnswer "First invalid Nr: " answer
+  return answer
 
-part2 :: Parsed Numbers -> IO ()
-part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "Not an answer: " answer
+findSummingSequence :: Integer
+                    -> Integer
+                    -> (Integer -> Seq.Seq Integer -> Seq.Seq Integer)
+                    -> Integer
+                    -> Seq.Seq Integer
+                    -> Seq.Seq Integer
+findSummingSequence p1 n next sum seq = case sum' `compare` p1 of
+  LT -> next sum' seq'
+  EQ -> seq'
+  GT -> case dropSummands sum' seq' of
+          (sum'', seq'') | sum'' == p1 -> seq''
+                         | otherwise -> next sum'' seq''
+  where
+    sum' = sum + n
+    seq' = seq Seq.|> n
+    dropSummands sum (x Seq.:<| xs) | sum - x <= p1 = (sum - x, xs)
+                                    | otherwise = dropSummands (sum - x) xs
+
+minMax :: Ord a => Seq.Seq a -> (a,a)
+minMax Seq.Empty = error "Min and max of an empty sequence don't exist."
+minMax (x Seq.:<| xs) = foldr envelop id xs (x,x)
+  where
+    envelop y next (min, max) | y < min = next (y, max)
+                              | y > max = next (min, y)
+                              | otherwise = next (min, max)
+
+weakness :: Integer -> Numbers -> Integer
+weakness p1 nrs = (uncurry (+)) . minMax $
+  foldr (findSummingSequence p1)
+        (\_ _ -> error "No encryption weakness")
+        nrs
+        0
+        Seq.Empty
+
+part2 :: Parsed Integer -> Parsed Numbers -> IO ()
+part2 p1 input = do
+  let answer = weakness <$> p1 <*> input
+  printAnswer "Encryption weakness: " answer
 
 main :: IO ()
 main = do
   let day =  "Day 9: Encoding Error"
   input <- readInput day
   putStrLn ""
-  part1 input
-  part2 input
+  p1 <- part1 input
+  part2 p1 input
   putStrLn ""
   defaultMain [
       bgroup "AoC"
         [ bench "Part 1" $ nfIO (silence $ readInput day >>= part1)
-        , bench "Part 2" $ nfIO (silence $ readInput day >>= part2)
+        , bench "Part 2" $ nfIO (silence $ readInput day >>= part2 p1)
         ]
     ]
