6bcc2a7a8ba6129f29623a8b0dbc05a025d6f32f
toonn <toonn@toonn.io>
2020-12-22 14:09:36 +0100

    Forgot to track AoC library

---
+++ 2020/AoC.hs
@@ -0,0 +1,30 @@
+module AoC where
+
+import Data.Void (Void)
+import System.FilePath ((</>))
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import Paths_AoC2020
+
+type Parser = Parsec Void String
+type Parsed a = Either (ParseErrorBundle String Void) a
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme (L.space hspace1 empty empty)
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+readInput :: String -> Parser a -> IO (Parsed a)
+readInput day parser = do
+  inputFile <- getDataFileName (day </> "input.txt")
+  parse parser inputFile
+    <$> readFile inputFile
+
+printAnswer :: Show a => String -> Parsed a -> IO ()
+printAnswer question answer =
+  either (putStrLn . errorBundlePretty)
+         (putStrLn . (question <>) . show)
+         answer
