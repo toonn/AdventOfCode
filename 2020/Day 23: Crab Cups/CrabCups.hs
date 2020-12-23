356a7381351647100725da363084583b789e3822
toonn <toonn@toonn.io>
2020-12-23 20:41:09 +0100

    Solve day 23 part 2 in 93 s

--- 2020/Day 23: Crab Cups/CrabCups.hs
+++ 2020/Day 23: Crab Cups/CrabCups.hs
@@ -10,6 +10,7 @@ import AoC
 
 import Data.Char (digitToInt, intToDigit, isDigit)
 import Data.Foldable (toList)
+import qualified Data.Map as M
 import qualified Data.Sequence as Seq
 
 type Cups = Seq.Seq Integer
@@ -18,21 +19,47 @@ cups :: Parser Cups
 cups = Seq.fromList . map (fromIntegral . digitToInt)
    <$> (takeWhile1P (Just "Digit") isDigit <* eol <* eof)
 
-destination :: Integer -> Cups -> Cups -> Integer
-destination 0 three cups = destination (maximum (three <> cups)) three cups
-destination cup three cups = case Seq.elemIndexL cup three of
-                               Nothing -> cup
-                               Just _ -> destination (cup - 1) three cups
+destination :: Integer -> Integer -> Cups -> Integer
+destination max 0 three = destination max max three
+destination max cup three = case Seq.elemIndexL cup three of
+  Nothing -> cup
+  Just _ -> destination max (cup - 1) three
 
-play :: Integer -> Cups -> Cups
-play 0 cups = cups
-play moves (current Seq.:<| cups) =
-  let (three, cups') = Seq.splitAt 3 cups
-      dest = destination (current - 1) three cups'
-      (before, after)
-        | Just index <- Seq.elemIndexL dest cups'
-          = Seq.splitAt (index + 1) cups'
-   in play (moves - 1) (before <> three <> after Seq.:|> current)
+pop :: Ord k => k -> M.Map k v -> (Maybe v, M.Map k v)
+pop = M.updateLookupWithKey (\_ _ -> Nothing)
+
+insert :: (M.Map Integer Cups, Cups) -> (M.Map Integer Cups, Cups)
+insert (inserts, (cup Seq.:<| cups)) = case insert (inserts', cups') of
+  (inserts'', cups'') -> (inserts'', cup Seq.:<| cups'')
+  where
+    (mCs, inserts') = pop cup inserts
+    cups' | Just cs <- mCs = cs <> cups
+          | otherwise = cups
+insert cups = cups
+
+play :: Integer -> Integer -> M.Map Integer Cups -> Cups -> Cups
+play _ 0 inserts cups = snd (insert (inserts, cups))
+play max moves inserts (current Seq.:<| cups) =
+  let
+      (inserts', c1 Seq.:<| cups')
+        | (Just cs, inserts') <- pop current inserts
+          = (inserts', cs <> cups)
+        | otherwise = (inserts, cups)
+      (inserts'', c2 Seq.:<| cups'')
+        | (Just cs, inserts'') <- pop c1 inserts'
+          = (inserts'', cs <> cups')
+        | otherwise = (inserts', cups')
+      (inserts''', c3 Seq.:<| cups''')
+        | (Just cs, inserts''') <- pop c2 inserts''
+          = (inserts''', cs <> cups'')
+        | otherwise = (inserts'', cups'')
+      three = Seq.fromList [c1,c2,c3]
+      dest = destination max (current - 1) three
+      (inserts'''', three') = insert (inserts''', three)
+      (three'', rest) = Seq.splitAt 3 three'
+      inserts''''' = M.insertWith (<>) dest three'' inserts''''
+      cups'''' = rest <> cups'''
+   in play max (moves - 1) inserts''''' (cups'''' Seq.:|> current)
 
 labels :: Cups -> String
 labels cups | Just index <- Seq.elemIndexL 1 cups =
@@ -42,13 +69,25 @@ labels cups | Just index <- Seq.elemIndexL 1 cups =
 
 part1 :: Parsed Cups -> IO ()
 part1 input = do
-  let answer = labels . play 100 <$> input
+  let answer = labels . (\cups -> play (maximum cups) 100 M.empty cups)
+           <$> input
   printAnswer "Labels after cup 1: " answer
+
+productTwo :: Cups -> Integer
+productTwo cups | Just index <- Seq.elemIndexL 1 cups =
+  let (x,y) = case (fromIntegral index + 2) `compare` Seq.length cups of
+        LT -> (Seq.index cups (index + 1), Seq.index cups (index + 2))
+        EQ -> (Seq.index cups (index + 1), Seq.index cups 0)
+        GT -> (Seq.index cups 0, Seq.index cups 1)
+   in x * y
 
 part2 :: Parsed Cups -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "Not an answer: " answer
+  let answer = productTwo
+             . play 1000000 10000000 M.empty
+             . (\cups -> cups <> Seq.fromList [maximum cups + 1..1000000])
+           <$> input
+  printAnswer "Product of two cups following label 1: " answer
 
 main :: IO ()
 main = do
