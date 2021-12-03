3c8ff5208406b03f93ea048718535bfa5cf2b18d
toonn <toonn@toonn.io>
2021-12-03 20:30:51 +0100

    Solve day 3 part 2 in 3.429 ms

--- 2021/Day 03: Binary Diagnostic/BinaryDiagnostic.hs
+++ 2021/Day 03: Binary Diagnostic/BinaryDiagnostic.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Criterion.Main
+import Data.Maybe (mapMaybe)
 import System.IO.Silently (silence)
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -39,13 +40,43 @@ binaryToDecimal = go 0
 
 part1 :: Parsed [Binary] -> IO ()
 part1 input = do
-  let answer = (\x -> binaryToDecimal x * binaryToDecimal (deriveEpsilonFromGamma x)) . deriveGamma <$> input
+  let answer = (\gamma -> binaryToDecimal gamma
+                        * binaryToDecimal (deriveEpsilonFromGamma gamma)
+               ) . deriveGamma <$> input
   printAnswer "Power consumption (Γ * ε): " answer
+
+bestMatch :: Char -> (Int -> Int -> Bool) -> [Binary] -> Binary
+bestMatch preference comparison numbers = go [] numbers
+  where
+    go :: Binary -> [Binary] -> Binary
+    go match [n] = match <> n
+    go match ns = case split  ns of
+      (b, ns') -> go (match <> [b]) ns'
+
+    split :: [Binary] -> (Char,[Binary])
+    split as =
+      case foldr (\(b:bs) (w, (ts,fs)) ->
+                   if preference == b
+                   then (w - 1, (bs:ts, fs)) else (w + 1, (ts, bs:fs))
+                 )
+                 (0, ([],[]))
+                 as of
+        (w, (ts, fs))
+          | 0 `comparison` w -> (preference, ts)
+          | w `comparison` 0 -> (if preference == '0' then '1' else '0', fs)
+          | otherwise -> (preference, ts)
+
+lifeSupportRating :: [Binary] -> Int
+lifeSupportRating numbers = oxygenGeneratorRating * cO2ScrubberRating
+  where
+    oxygenGeneratorRating = binaryToDecimal (bestMatch '1' (>) numbers)
+    cO2ScrubberRating = binaryToDecimal (bestMatch '0' (<) numbers)
+
 
 part2 :: Parsed [Binary] -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = lifeSupportRating <$> input
+  printAnswer "Life support rating: " answer
 
 main :: IO ()
 main = do
