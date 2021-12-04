c3fe512e625be69e92a7beb46422022ce11eadbd
toonn <toonn@toonn.io>
2021-12-04 21:31:00 +0100

    Solve day 4 part 1 in 5.488 ms

---
+++ 2021/Day 04: Giant Squid/GiantSquid.hs
@@ -0,0 +1,114 @@
+module Main where
+
+import Criterion.Main
+import System.IO.Silently (silence)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Data.IntMap as M
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import AoC
+
+type Bingo = M.IntMap (Int, Int)
+type Seen = M.IntMap Int
+
+row :: Parser [Int]
+row = hspace *> manyTill integer eol
+
+board :: Parser Bingo
+board = do
+  numbers <- manyTill row (eol *> pure () <|> lookAhead eof)
+  let board = foldr
+        (\r next b y ->
+          next (foldr (\n next' b' x -> next' (M.insert n (x,y) b') (x+1))
+                      const
+                      r
+                      b
+                      0
+               )
+               (y+1)
+        )
+        const
+        numbers
+        M.empty
+        0
+  pure board
+
+bingoSetup :: Parser ([Int], [Bingo])
+bingoSetup = do
+  draws <- sepBy integer (char ',')
+  eol
+  eol
+  boards <- manyTill board eof
+  pure (draws, boards)
+
+winningBoard :: ([Int], [Bingo]) -> (Int, Bingo)
+winningBoard (draws, boards) =
+  go 0 draws (map (\b -> (zeroes, zeroes, b)) boards)
+  where
+    zeroes = M.fromAscList (map (\a -> (a, 0)) [0..4])
+
+    go :: Int -> [Int] -> [(Seen, Seen, Bingo)] -> (Int, Bingo)
+    go last (d:ds) bs = case foldr (\(xs, ys, b) m ->
+                                case M.foldr (\count m ->
+                                               if count == 5
+                                               then Just b
+                                               else m
+                                             )
+                                             m
+                                             xs of
+                                  Nothing -> M.foldr (\count m ->
+                                                       if count == 5
+                                                       then Just b
+                                                       else m
+                                                     )
+                                                     m
+                                                     ys
+                                  Just b -> Just b
+                              )
+                              Nothing
+                              bs of
+      Just b -> (last,b)
+      Nothing -> go
+        d
+        ds
+        (map (\(xs, ys, b) ->
+               case M.updateLookupWithKey (\_ _ -> Nothing) d b of
+                 (Nothing, b') -> (xs, ys, b')
+                 (Just (x,y), b') ->
+                   ( M.adjust (+ 1) x xs
+                   , M.adjust (+ 1) y ys
+                   , b'
+                   )
+             )
+             bs
+        )
+
+score :: (Int, Bingo) -> Int
+score (n, b) = n * (sum . M.keys $ b)
+
+part1 :: Parsed ([Int], [Bingo]) -> IO ()
+part1 input = do
+  let answer = score . winningBoard <$> input
+  printAnswer "Final score of the winning board: " answer
+
+part2 :: Parsed ([Int], [Bingo]) -> IO ()
+part2 input = do
+  let answer = const "P" <$> input
+  printAnswer "No answer yet: " answer
+
+main :: IO ()
+main = do
+  let day = "Day 04: Giant Squid"
+  let parser = bingoSetup
+  input <- readInput day parser
+  putStrLn ""
+  part1 input
+  part2 input
+  putStrLn ""
+  defaultMain [
+      bgroup "AoC"
+        [ bench "Part 1" $ nfIO (silence $ readInput day parser >>= part1)
+        , bench "Part 2" $ nfIO (silence $ readInput day parser >>= part2)
+        ]
+    ]
