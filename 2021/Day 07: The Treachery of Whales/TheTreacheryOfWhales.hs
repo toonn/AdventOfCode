259d7ee861f8ed364b9e9fa9afd8b695f35af2ba
toonn <toonn@toonn.io>
2021-12-07 22:52:35 +0100

    Solve day 7 part 2 in 1.308 ms

--- 2021/Day 07: The Treachery of Whales/TheTreacheryOfWhales.hs
+++ 2021/Day 07: The Treachery of Whales/TheTreacheryOfWhales.hs
@@ -40,10 +40,50 @@ part1 input = do
   let answer = (\crabs -> fuelToAlign (leftLeaningMedian crabs) crabs) <$> input
   printAnswer "Fuel to align to optimal position: " answer
 
+average :: Crabs -> Int
+average crabs = round
+  ( fromIntegral (M.foldrWithKey (\k freq total -> k * freq + total) 0 crabs)
+  / fromIntegral (sum crabs)
+  )
+
+triangularNumber :: Int -> Int
+triangularNumber n = round (n' * (n' + 1) / 2)
+  where n' = fromIntegral n
+
+rampingFuelToAlign :: Int -> Crabs -> Int
+rampingFuelToAlign location = M.foldrWithKey
+  (\loc number fuel ->
+    triangularNumber (abs (location - loc)) * number + fuel
+  )
+  0
+
+descend :: Int -> Crabs -> Int
+descend location crabs = go l f l' f'
+  where
+    locationFuel = rampingFuelToAlign location crabs
+    location' = location - 1
+    locationFuel' = rampingFuelToAlign location' crabs
+    (l,f,l',f') | locationFuel < locationFuel' = ( location'
+                                                 , locationFuel'
+                                                 , location
+                                                 , locationFuel
+                                                 )
+                | otherwise = ( location
+                              , locationFuel
+                              , location'
+                              , locationFuel'
+                              )
+
+    go loc fuel loc' fuel' | fuel < fuel' = fuel
+                           | otherwise = go loc' fuel' loc'' fuel''
+      where
+        loc'' = 2 * loc' - loc
+        fuel'' = rampingFuelToAlign loc'' crabs
+
 part2 :: Parsed Crabs -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = (\crabs -> descend (average crabs) crabs) <$> input
+  printAnswer "Fuel to align according to crab engineering: " answer
 
 main :: IO ()
 main = do
