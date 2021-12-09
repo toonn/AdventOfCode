cf0a46f6de2948eb35c68a69b276f76e44cafb28
toonn <toonn@toonn.io>
2021-12-09 23:39:38 +0100

    Solve day 9 part 2 in 29.61 ms

--- 2021/Day 09: Smoke Basin/SmokeBasin.hs
+++ 2021/Day 09: Smoke Basin/SmokeBasin.hs
@@ -6,11 +6,17 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+
+import Control.Monad (guard)
 import Data.Char (digitToInt)
-import Data.List (zip4, zip5)
+import Data.List (sortOn, zip5, zip6)
+import Data.Maybe (mapMaybe)
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 import AoC
 
+type Point = (Int, Int, Int)
 type HeightMap = [[Int]]
 
 digits :: Parser [Int]
@@ -19,48 +25,91 @@ digits = map digitToInt <$> manyTill digitChar eol
 parser :: Parser HeightMap
 parser = manyTill digits eof
 
-lowPoints :: HeightMap -> [Int]
-lowPoints (r:rs) = go Nothing r rs []
+lowPoints :: HeightMap -> [Point]
+lowPoints (r:rs) = go 0 Nothing r rs []
   where
-    go (Just as) bs []      lowPs =
-      foldr (\(a, b', b, b'') lPs ->
+    go y (Just as) bs []      lowPs =
+      foldr (\(x, a, b', b, b'') lPs ->
               if b < minimum [a,b',b'']
-              then b:lPs
+              then (b,x,y):lPs
               else lPs
             )
             lowPs
-            (zip4 as (10:bs) bs (tail bs <> [10]))
-    go Nothing   bs (cs:rs) lowPs =
-      go (Just bs) cs rs (foldr (\(b', b, b'', c) lPs ->
-                                  if b < minimum [b',b'',c]
-                                  then b:lPs
-                                  else lPs
-                                )
-                                lowPs
-                                (zip4 (10:bs) bs (tail bs <> [10]) cs)
-                         )
-    go (Just as) bs (cs:rs) lowPs =
-      go (Just bs) cs rs (foldr (\(a, b', b, b'', c) lPs ->
-                                  if b < minimum [a,b',b'',c]
-                                  then b:lPs
-                                  else lPs
-                                )
-                                lowPs
-                                (zip5 as (10:bs) bs (tail bs <> [10]) cs)
-                         )
+            (zip5 [0..] as (10:bs) bs (tail bs <> [10]))
+    go y Nothing   bs (cs:rs) lowPs =
+      go (y + 1)
+         (Just bs)
+         cs
+         rs
+         (foldr (\(x, b', b, b'', c) lPs ->
+                  if b < minimum [b',b'',c]
+                  then (b,x,y):lPs
+                  else lPs
+                )
+                lowPs
+                (zip5 [0..] (10:bs) bs (tail bs <> [10]) cs)
+         )
+    go y (Just as) bs (cs:rs) lowPs =
+      go (y + 1)
+         (Just bs)
+         cs
+         rs
+         (foldr (\(x, a, b', b, b'', c) lPs ->
+                  if b < minimum [a,b',b'',c]
+                  then (b,x,y):lPs
+                  else lPs
+                )
+                lowPs
+                (zip6 [0..] as (10:bs) bs (tail bs <> [10]) cs)
+         )
 
-riskLevel :: Int -> Int
-riskLevel = (+ 1)
+riskLevel :: Point -> Int
+riskLevel (height, _, _) = height + 1
 
 part1 :: Parsed HeightMap -> IO ()
 part1 input = do
   let answer = sum . map riskLevel . lowPoints <$> input
   printAnswer "Sum of low point risk levels: " answer
 
+heightMap :: HeightMap -> M.Map (Int, Int) Int
+heightMap rows =
+  M.fromList (foldr (\row next y ->
+                      filter (\(_, h) -> h /= 9)
+                             (zip (zip [0..] (repeat y)) row <> next (y + 1))
+                    )
+                    (const [])
+                    rows
+                    0
+             )
+
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors (x,y) = [(x-1,y), (x,y-1), (x,y+1), (x+1,y)]
+
+higherNeighbors :: M.Map (Int, Int) Int -> Point -> S.Set Point
+higherNeighbors hM (h, x, y) =
+  S.fromList (mapMaybe (\(x',y') ->
+                         case M.lookup (x',y') hM of
+                           Nothing -> Nothing
+                           Just h' | h' >= h -> Just (h',x',y')
+                                   | otherwise -> Nothing
+                       )
+                       (neighbors (x,y))
+             )
+
+basin :: M.Map (Int, Int) Int -> S.Set Point -> S.Set Point -> S.Set Point
+basin hM ps qs | S.null qs = ps
+               | otherwise = basin hM border (border S.\\ ps)
+  where
+    border = S.foldr (\q ns -> higherNeighbors hM q `S.union` ns) ps qs
+
+basins :: M.Map (Int, Int) Int -> [Point] -> [S.Set Point]
+basins hM lowPs = map ((\s -> basin hM s s) . S.singleton) lowPs
+
 part2 :: Parsed HeightMap -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = product . take 3 . sortOn negate . map S.size . (\heights -> basins (heightMap heights) (lowPoints heights))
+           <$> input
+  printAnswer "Product of three largest basin sizes: " answer
 
 main :: IO ()
 main = do
