85b0c3db204d4b9d1ee1b8b664216dc606400e09
toonn <toonn@toonn.io>
2021-12-10 17:15:30 +0100

    Solve day 10 part 2 in 3.189 ms

--- 2021/Day 10: Syntax Scoring/SyntaxScoring.hs
+++ 2021/Day 10: Syntax Scoring/SyntaxScoring.hs
@@ -6,6 +6,7 @@ import Text.Megaparsec hiding (chunk)
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import Data.List (sort)
 import Data.Maybe (mapMaybe)
 
 import AoC
@@ -18,35 +19,57 @@ chunk = manyTill (satisfy (`elem` "()[]{}<>")) eol
 parser :: Parser [Chunk]
 parser = manyTill chunk eof
 
-score :: Char -> Int
-score ')' = 3
-score ']' = 57
-score '}' = 1197
-score '>' = 25137
-
-corrupt :: Chunk -> Maybe Char
+corrupt :: Chunk -> ([Char], Maybe Char)
 corrupt = go []
   where
-    go [] [] = Nothing
-    go (e:_) [] = Nothing
+    go [] [] = ([], Nothing)
+    go (e:es) [] = (e:es , Nothing)
     go expect (c:cs) | c == '(' = go (')':expect) cs
                      | c == '[' = go (']':expect) cs
                      | c == '{' = go ('}':expect) cs
                      | c == '<' = go ('>':expect) cs
                      | c `elem` ")]}>" = case expect of
                                            (e:es) | c == e -> go es cs
-                                                  | otherwise -> Just c
-                                           _ -> Nothing
+                                                  | otherwise -> ([], Just c)
+                                           es -> (es, Nothing)
+
+errorScore :: Char -> Int
+errorScore ')' = 3
+errorScore ']' = 57
+errorScore '}' = 1197
+errorScore '>' = 25137
 
 part1 :: Parsed [Chunk] -> IO ()
 part1 input = do
-  let answer = sum . map score . mapMaybe corrupt <$> input
+  let answer = sum . map errorScore . mapMaybe (snd . corrupt) <$> input
   printAnswer "Corrupted chunk syntax score: " answer
+
+completionScore :: [Char] -> Int
+completionScore = go 0
+  where
+    points :: Char -> Int
+    points ')' = 1
+    points ']' = 2
+    points '}' = 3
+    points '>' = 4
+
+    go s [] = s
+    go s (c:cs) = go (5 * s + points c) cs
+
+middle :: [Int] -> Int
+middle xs = head (drop (length xs `quot` 2) (sort xs))
 
 part2 :: Parsed [Chunk] -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = middle
+             . map completionScore
+             . mapMaybe ((\(es, _) -> case es of
+                           [] -> Nothing
+                           es -> Just es
+                         ) . corrupt
+                        )
+           <$> input
+  printAnswer "Middle score of completions: " answer
 
 main :: IO ()
 main = do
