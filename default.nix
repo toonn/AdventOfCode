b39bee5cf7120cd7360dd71a4bad145be619b799
toonn <toonn@toonn.io>
2022-12-01 14:42:02 +0100

    Update project files for GHC 9.2.5
    Updating GHC means the previous years are no longer compatible due to
    constraints on `base`. I've restricted projects built to the current
    year to avoid the problem but should consider splitting the project into
    several.

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import ~/src/nix-config/haskell.nix/nixpkgs.nix
-, compiler-nix-name ? "ghc8107"
+, compiler-nix-name ? "ghc925"
 }:
 pkgs.haskell-nix.project {
   inherit compiler-nix-name;
--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { pkgs ? import ~/src/nix-config/haskell.nix/nixpkgs.nix
-, compiler-nix-name ? "ghc8107"
+, compiler-nix-name ? "ghc925"
 , shell ? import ~/src/nix-config/haskell.nix/shell.nix
 }:
 let hsPkgs = import ./default.nix { inherit pkgs compiler-nix-name; };
-    stackage = pkgs.haskell-nix.snapshots."lts-18.18";
+    stackage = pkgs.haskell-nix.snapshots."lts-20.2";
 in shell { inherit hsPkgs;
-           for = with hsPkgs; [ AoC2021 ];
+           for = with hsPkgs; [ AoC2022 ];
            buildInputs = (map (p: stackage."${p}".components.exes."${p}")
                               [ "pandoc" ]
                          ) ++ (with (import <nixpkgs> {}); [ z3 ]);
