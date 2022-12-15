43fcc5a91aa6a42018c16d92e0693034ed69bdb7
toonn <toonn@toonn.io>
2022-12-15 22:31:49 +0100

    Solve day 15 part 2 in 110.2 s

--- 2022/Day 15: Beacon Exclusion Zone/BeaconExclusionZone.hs
+++ 2022/Day 15: Beacon Exclusion Zone/BeaconExclusionZone.hs
@@ -6,7 +6,13 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import qualified Data.ExtendedReal as ER
+import qualified Data.IntegerInterval as II
+import qualified Data.IntervalSet as IS
+import Data.List (find)
 import qualified Data.Map as M
+import qualified Data.Map.Merge.Lazy as M
+import Data.Maybe (fromJust)
 import qualified Data.Set as S
 
 import AoC
@@ -60,10 +66,66 @@ part1 input = do
   let answer = S.size . beaconExclusion 2000000 <$> input
   printAnswer "Positions excluding beacon when y = 2M: " answer
 
+tuningFrequency :: Coord -> Int
+tuningFrequency (x,y) = 4000000 * x + y
+
+beaconLocation :: Int -> Int -> Sensors -> Coord
+beaconLocation lBound hBound sensors
+  = M.foldrWithKey
+      (\(x,y) b more intervals ->
+        let r = manhattan (x,y) b
+            lower = let l = lBound - y
+                     in signum l * (min (abs l) r)
+            upper = let h = hBound - y
+                     in signum h * (min (abs h) r)
+            exclusionZone
+              = M.fromAscList
+              . map (\dy ->
+                      let dx = r - abs dy
+                       in ( y + dy
+                          , II.toInterval
+                              (finite (x - dx) II.<=..<= finite (x + dx))
+                          )
+                    )
+              $ [lower..upper]
+            intervals'
+              = M.merge M.preserveMissing
+                        (M.mapMissing (\_ exInt ->
+                                        ( IS.delete exInt
+                                        . IS.singleton
+                                        . II.toInterval
+                                        )
+                                        (finite lBound II.<=..<= finite hBound)
+                                      )
+                        )
+                        (M.zipWithMatched (\_ inSet exInt ->
+                                            IS.delete exInt inSet
+                                          )
+                        )
+                        intervals
+                        exclusionZone
+        in more intervals'
+      )
+      (M.foldrWithKey (\y inSet next ->
+                        let ins = map II.fromInterval . IS.toList $ inSet
+                            beaconInts = filter (not . II.null) ins
+                            beacon | [beaconInt] <- beaconInts
+                                   , Just x <- II.pickup beaconInt
+                                   = (fromIntegral x, y)
+                                   | otherwise = next
+                         in beacon
+                      )
+                      (error "No hidden beacon")
+      )
+      sensors
+      M.empty
+  where
+    finite = ER.Finite . fromIntegral
+
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = tuningFrequency . beaconLocation 0 4000000 <$> input
+  printAnswer "Tuning frequency of distress beacon: " answer
 
 main :: IO ()
 main = do
