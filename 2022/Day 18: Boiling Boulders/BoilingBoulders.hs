ec4420842f279ccb9b33e1497bacaaba692d562a
toonn <toonn@toonn.io>
2022-12-18 21:45:50 +0100

    Solve day 18 part 2 in 93.61 ms

--- 2022/Day 18: Boiling Boulders/BoilingBoulders.hs
+++ 2022/Day 18: Boiling Boulders/BoilingBoulders.hs
@@ -50,10 +50,70 @@ part1 input = do
   let answer = surfaceArea <$> input
   printAnswer "Surface area of scanned lava droplet: " answer
 
+air :: Coord -> Coord -> Droplet -> Droplet
+air (minX,minY,minZ) (maxX,maxY,maxZ) droplet
+  = S.fromAscList [ (x,y,z)
+                  | x <- [minX..maxX]
+                  , y <- [minY..maxY]
+                  , z <- [minZ..maxZ]
+                  ]
+  S.\\ droplet
+
+exteriorVolume :: Droplet -> Droplet
+exteriorVolume droplet = go volumeStart S.empty
+  where
+    xs = S.map (\(x,y,z) -> x) droplet
+    minX = S.findMin xs - 1
+    maxX = S.findMax xs + 1
+    ys = S.map (\(x,y,z) -> y) droplet
+    minY = S.findMin ys - 1
+    maxY = S.findMax ys + 1
+    zs = S.map (\(x,y,z) -> z) droplet
+    minZ = S.findMin zs - 1
+    maxZ = S.findMax zs + 1
+
+    volumeStart = S.fromAscList [ (x,y,z)
+                                | x <- [minX,maxX]
+                                , y <- [minY,maxY]
+                                , z <- [minZ,maxZ]
+                                ]
+
+    airVolume = air (minX,minY,minZ) (maxX,maxY,maxZ) droplet
+
+    inBounds (x,y,z) = minX <= x && x <= maxX
+                    && minY <= y && y <= maxY
+                    && minZ <= z && z <= maxZ
+
+    go edges seen | S.null edges = seen
+                  | otherwise
+                  = let seen' = seen `S.union` edges
+                        candidates = S.foldr (\c s ->
+                                               S.fromAscList
+                                                 (filter inBounds (neighbors c))
+                                               `S.union` s
+                                             )
+                                             S.empty
+                                             edges
+                        edges' = S.intersection (candidates S.\\ seen')
+                                                airVolume
+                     in go edges' seen'
+
+exteriorSurfaceArea :: Droplet -> Int
+exteriorSurfaceArea droplet
+  = S.foldr (\c area ->
+              area + S.size (S.intersection (S.fromAscList (neighbors c))
+                                            exterior
+                            )
+            )
+            0
+            droplet
+  where
+    exterior = exteriorVolume droplet
+
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer "No answer yet: " answer
+  let answer = exteriorSurfaceArea <$> input
+  printAnswer "Exterior surface area of scanned lava droplet: " answer
 
 main :: IO ()
 main = do
