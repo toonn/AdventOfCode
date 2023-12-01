78ae89b39d94d35e8531612269201039a1a2b7bd
toonn <toonn@toonn.io>
2023-12-01 23:47:38 +0100

    Update project files for GHC 9.4.8
    Updating GHC means the previous years are no longer compatible due to
    constraints on `base`. I've restricted projects built to the current
    year to avoid the problem but should consider splitting the project into
    several.

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import ~/src/nix-config/haskell.nix/nixpkgs.nix
-, compiler-nix-name ? "ghc925"
+, compiler-nix-name ? "ghc948"
 }:
 pkgs.haskell-nix.project {
   inherit compiler-nix-name;
--- shell.nix
+++ shell.nix
@@ -1,12 +1,26 @@
 { pkgs ? import ~/src/nix-config/haskell.nix/nixpkgs.nix
-, compiler-nix-name ? "ghc925"
+, compiler-nix-name ? "ghc948"
 , shell ? import ~/src/nix-config/haskell.nix/shell.nix
 }:
-let hsPkgs = import ./default.nix { inherit pkgs compiler-nix-name; };
-    stackage = pkgs.haskell-nix.snapshots."lts-20.2";
-in shell { inherit hsPkgs;
-           for = with hsPkgs; [ AoC2022 ];
-           buildInputs = (map (p: stackage."${p}".components.exes."${p}")
-                              [ "pandoc" ]
-                         ) ++ (with (import <nixpkgs> {}); [ z3 ]);
-         }
+let inherit (pkgs.lib) mapAttrs;
+    inherit (pkgs.haskell-nix) hackage-package;
+    hsPkgs = import ./default.nix { inherit pkgs compiler-nix-name; };
+    stackage = pkgs.haskell-nix.snapshots."lts-21.22";
+in shell {
+  inherit hsPkgs;
+  for = with hsPkgs; [ AoC2023 ];
+  buildInputs = let hackageFromExe =
+                      mapAttrs (_: p: { inherit compiler-nix-name;
+                                        index-state = "2023-11-30T00:00:00Z";
+                                      } // p)
+                               { pandoc = { name = "pandoc-cli";
+                                            version = "0.1.1.1";
+                                          };
+                               };
+                    hackages = map (exe:
+                                      ( hackage-package hackageFromExe."${exe}"
+                                      ).components.exes."${exe}"
+                                   );
+    in ( hackages [ "pandoc" ]
+       ) ++ (with (import <nixpkgs> {}); [ z3 ]);
+}
