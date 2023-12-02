22d35125bb4e334c1b33873b31af7ba957277788
toonn <toonn@toonn.io>
2023-12-02 01:30:22 +0100

    Solve day 1 part 2 in 76.4 ms

--- 2023/Day 01: Trebuchet?!/Trebuchet.hs
+++ 2023/Day 01: Trebuchet?!/Trebuchet.hs
@@ -6,7 +6,10 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import Control.Applicative (asum)
 import Data.Char (digitToInt, isAlphaNum, isDigit)
+import Data.List (find)
+import Data.Maybe (fromMaybe, fromJust)
 
 import AoC
 
@@ -36,10 +39,88 @@ part1 input = do
   let answer = sum . map calibrationValues <$> input
   printAnswer "Sum of calibration values: " answer
 
+unspellDigit :: String -> String
+unspellDigit "one"   = "1"
+unspellDigit "two"   = "2"
+unspellDigit "three" = "3"
+unspellDigit "four"  = "4"
+unspellDigit "five"  = "5"
+unspellDigit "six"   = "6"
+unspellDigit "seven" = "7"
+unspellDigit "eight" = "8"
+unspellDigit "nine"  = "9"
+
+unspellPostfix :: Line -> String -> Maybe Line
+unspellPostfix initials postfix =
+  case splitAt (length initials - length postfix) initials of
+    (is, postfix') | postfix == postfix' -> Just (is <> (unspellDigit postfix))
+    _ -> Nothing
+
+unspellForward :: Line -> Line
+unspellForward l = foldr (\c more initials ->
+                          let initials' = initials <> [c]
+                           in more
+                            . fromMaybe initials'
+                            $ ( asum
+                              . map (unspellPostfix initials')
+                              $ [ "one"
+                                , "two"
+                                , "three"
+                                , "four"
+                                , "five"
+                                , "six"
+                                , "seven"
+                                , "eight"
+                                , "nine"
+                                ]
+                              )
+                        )
+                        id
+                        l
+                        ""
+
+firstDigit :: Line -> Int
+firstDigit = digitToInt . fromMaybe '0' . find isDigit . unspellForward
+
+lastDigit :: Line -> Int
+lastDigit l = digitToInt
+            . fromMaybe '0'
+            . fst
+            . foldr (\c (last, rest) ->
+                      let (c', rest') | "one" == take 3 (c:rest)
+                                      = ('1', drop 3 rest)
+                                      | "two" == take 3 (c:rest)
+                                      = ('2', drop 3 rest)
+                                      | "three" == take 5 (c:rest)
+                                      = ('3', drop 5 rest)
+                                      | "four" == take 4 (c:rest)
+                                      = ('4', drop 4 rest)
+                                      | "five" == take 4 (c:rest)
+                                      = ('5', drop 4 rest)
+                                      | "six" == take 3 (c:rest)
+                                      = ('6', drop 3 rest)
+                                      | "seven" == take 5 (c:rest)
+                                      = ('7', drop 5 rest)
+                                      | "eight" == take 5 (c:rest)
+                                      = ('8', drop 5 rest)
+                                      | "nine" == take 4 (c:rest)
+                                      = ('9', drop 4 rest)
+                                      | otherwise
+                                      = (c, rest)
+                          last' | last == Nothing && isDigit c' = Just c'
+                                | otherwise = last
+                       in (last', c':rest')
+                    )
+                    (Nothing, "")
+            $ l
+
+spelledCalibrationValues :: Line -> Int
+spelledCalibrationValues l = 10 * firstDigit l + lastDigit l
+
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const "P" <$> input
-  printAnswer ": " answer
+  let answer = sum . map spelledCalibrationValues <$> input
+  printAnswer "Sum of unspelled calibration values: " answer
 
 main :: IO ()
 main = do
---
+++ 2023/Day 02: Cube Conundrum/CubeConundrum.hs
@@ -0,0 +1,83 @@
+module Main where
+
+import Criterion.Main
+import System.IO.Silently (silence)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import qualified Data.IntMap as IM
+import Data.Char (digitToInt, isAlphaNum, isDigit)
+
+import AoC
+
+type ID = Int
+type Draw = Int
+type Set = (Draw, Draw, Draw)
+type Draws = [Draw]
+type Game = (Draws, Draws, Draws)
+
+type Input = IM.IntMap Game
+
+draw :: Parser Set
+draw = do
+  n <- integer
+  inT <- lexeme ( (string "red" *> pure (\a -> (a,0,0)))
+                <|> (string "green" *> pure (\b -> (0,b,0)))
+                <|> (string "blue" *> pure (\c -> (0,0,c)))
+                )
+  pure $ inT n
+
+(+.) :: Num a => (a, a, a) -> (a, a, a) -> (a, a, a)
+(a,b,c) +. (a',b',c') = (a + a', b + b', c + c')
+
+set :: Parser Set
+set = foldr (+.) (0,0,0) <$> sepBy draw (lexeme (char ','))
+
+game :: Parser (ID, Game)
+game = do
+  lexeme (string "Game")
+  iD <- lexeme integer
+  lexeme (char ':')
+  g <- unzip3 <$> sepBy set (lexeme (char ';'))
+  pure (iD, g)
+
+parser :: Parser Input
+parser = IM.fromAscList <$> sepEndBy game eol <* eof
+
+mostSeen :: Game -> Set
+mostSeen (rs, gs, bs) = (maximum rs, maximum gs, maximum bs)
+
+possible :: Set -> Bool
+possible (r,g,b) = r <= 12 && g <= 13 && b <= 14
+
+filterPossible :: Input -> IM.IntMap Set
+filterPossible = IM.filter possible . IM.map mostSeen
+
+sumPossibleIDs :: Input -> Int
+sumPossibleIDs = sum . IM.keys . filterPossible
+
+part1 :: Parsed Input -> IO ()
+part1 input = do
+  let answer = sumPossibleIDs <$> input
+  printAnswer "Sum of possible game IDs: " answer
+
+part2 :: Parsed Input -> IO ()
+part2 input = do
+  let answer = const "P" <$> input
+  printAnswer "No answer yet: " answer
+
+main :: IO ()
+main = do
+  let day = "Day 02: Cube Conundrum"
+  input <- readInput day parser
+  putStrLn ""
+  part1 input
+  part2 input
+  putStrLn ""
+  defaultMain [
+      bgroup "AoC"
+        [ bench "Part 1" $ nfIO (silence $ readInput day parser >>= part1)
+        , bench "Part 2" $ nfIO (silence $ readInput day parser >>= part2)
+        ]
+    ]
