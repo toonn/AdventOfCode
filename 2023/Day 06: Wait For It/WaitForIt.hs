7559d816d20b73a441a84e6a3a39b6119f396c36
toonn <toonn@toonn.io>
2023-12-06 09:56:59 +0100

    Solve day 6 part 1 in 166.6 μs
    For some reason the benchmarking for part 2, where my provisional
    solution is `const 'P' <$> input` makes my system unresponsive. This
    doesn't normally happen. I should figure out why and whether it's
    fixable.

---
+++ 2023/Day 06: Wait For It/WaitForIt.hs
@@ -0,0 +1,61 @@
+module Main where
+
+import Criterion.Main
+import System.IO.Silently (silence)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import AoC
+
+type Input = [(Int, Int)]
+
+times :: Parser [Int]
+times = lexeme (string "Time:") *> many integer
+
+distances :: Parser [Int]
+distances = lexeme (string "Distance:") *> many integer
+
+parser :: Parser Input
+parser = zip <$> (times <* eol) <*> (distances <* eol) <* eof
+
+nrWays :: (Int, Int) -> Int
+nrWays (time, distance) =
+  let squaredHalfDistance = time ^ 2 - 4 * distance
+      halfDistance = sqrt
+                   . fromIntegral
+                   $ squaredHalfDistance
+      solutionL = floor
+                . (+ 1)
+                $ (fromIntegral time - halfDistance) / 2
+      solutionR = ceiling
+                . (subtract 1)
+                $ (fromIntegral time + halfDistance) / 2
+      ways | squaredHalfDistance <= 0 = 0
+           | otherwise = solutionR - solutionL + 1
+   in ways
+
+part1 :: Parsed Input -> IO ()
+part1 input = do
+  let answer = product . map nrWays <$> input
+  printAnswer "Product of number of ways to win: " answer
+
+part2 :: Parsed Input -> IO ()
+part2 input = do
+  let answer = const 'P' <$> input
+  printAnswer "No answer yet: " answer
+
+main :: IO ()
+main = do
+  let day = "Day 06: Wait For It"
+  input <- readInput day parser
+  putStrLn ""
+  part1 input
+  part2 input
+  putStrLn ""
+  defaultMain [
+      bgroup "AoC"
+        [ bench "Part 1" $ nfIO (silence $ readInput day parser >>= part1)
+        , bench "Part 2" $ nfIO (silence $ readInput day parser >>= part2)
+        ]
+    ]
