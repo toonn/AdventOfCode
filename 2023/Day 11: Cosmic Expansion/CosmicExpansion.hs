32522f2c5f967006fcdc885bbec71a0244e5b0aa
toonn <toonn@toonn.io>
2023-12-11 10:38:37 +0100

    Solve day 11 part 2 in 20.7 ms

--- 2023/Day 11: Cosmic Expansion/CosmicExpansion.hs
+++ 2023/Day 11: Cosmic Expansion/CosmicExpansion.hs
@@ -24,13 +24,14 @@ parser :: Parser Input
 parser = S.fromAscList . concat . zipWith (map . (,)) [0..]
      <$> sepEndBy1 row eol <* eof
 
-expand :: Input -> Input
-expand galaxies = S.map (\(y,x) ->
-                          let y' = y + length (filter (< y) emptyYs)
-                              x' = x + length (filter (< x) emptyXs)
-                           in (y',x')
-                        )
-                        galaxies
+expand :: Int -> Input -> Input
+expand expansion galaxies =
+  S.map (\(y,x) ->
+          let y' = y + (expansion - 1) * length (filter (< y) emptyYs)
+              x' = x + (expansion - 1) * length (filter (< x) emptyXs)
+           in (y',x')
+        )
+        galaxies
   where
     ys = S.map fst galaxies
     (minY,maxY) = (S.findMin ys, S.findMax ys)
@@ -46,13 +47,15 @@ pairs (g:galaxies) = map ((,) g) galaxies <> pairs galaxies
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = sum . map (uncurry manhattan) . pairs . S.toList . expand <$> input
+  let answer = sum . map (uncurry manhattan) . pairs . S.toList . expand 2
+           <$> input
   printAnswer "Sum of all pairwise shortest paths: " answer
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = sum . map (uncurry manhattan) . pairs . S.toList . expand 1000000
+           <$> input
+  printAnswer "Sum of pairwise shortest paths in older universe: " answer
 
 main :: IO ()
 main = do
