e69dc326ef6ba5af3af8f516bbb4327ffa0089a3
toonn <toonn@toonn.io>
2023-12-13 17:11:46 +0100

    Solve day 12 part 2 in 2.4 s

--- 2023/Day 12: Hot Springs/HotSprings.hs
+++ 2023/Day 12: Hot Springs/HotSprings.hs
@@ -8,9 +8,12 @@ import qualified Text.Megaparsec.Char.Lexer as L
 
 import AoC
 
-import Data.List (find, partition, tails)
+import Control.Arrow (first)
+import Data.List (intercalate, tails)
+import qualified Data.Map as M
 
 type SpringState = ([Char], [Int])
+type BinnedSpringState = ([[Char]], [Int])
 
 type Input = [SpringState]
 
@@ -24,54 +27,76 @@ springState = do
 parser :: Parser Input
 parser = sepEndBy1 springState eol <* eof
 
-nrArrangements :: SpringState -> Int
-nrArrangements (springs, [])
-  | any (== '#') springs = 0 -- Any broken springs would have to be part of a
-                             -- contiguous group
-  | otherwise = 1
-nrArrangements (springs, size:groupSizes)
-  | springs' <- dropWhile (== '.') springs
-  , length springs' >= size + sum groupSizes + length groupSizes
-  = let recurseNr | ('?':rest) <- springs'
-                  = nrArrangements (rest, size : groupSizes)
-                  | otherwise
-                  = 0
-        (prefix, postfix) = splitAt size springs'
-        nr | any (== '.') prefix
-           = let rNr | (n:prefix') <- prefix
-                     , n /= '#'
-                     , Just prefix'' <-
-                         find (all (/= '.'))
-                       . (\(fine, potential) ->
-                           let pF | (p:_) <- potential = [p]
-                                  | otherwise = []
-                            in fine <> pF
-                         )
-                       . span (\l -> null l || head l /= '#')
-                       . tails
-                       $ prefix'
-                     = nrArrangements (prefix'' <> postfix, size:groupSizes)
-                     | otherwise
-                     = 0
-              in rNr
-           | (n:postfix') <- postfix
-           = let reducedNr | n == '#' = 0
-                           | otherwise = nrArrangements (postfix', groupSizes)
-              in recurseNr + reducedNr
-           | [] <- postfix, [] <- groupSizes
-           = 1
-     in nr
-  | otherwise = 0
+bins :: [Char] -> [[Char]]
+bins springs =
+  foldr (\spring more g ->
+          let next | spring /= '.' || null g = id
+                   | otherwise = (g:)
+              g' | spring == '.' = []
+                 | otherwise = g <> [spring]
+           in next (more g')
+        )
+        (\g -> let l | null g = []
+                     | otherwise = [g]
+                in l
+        )
+        springs
+        []
+
+arrangements :: M.Map BinnedSpringState Int -> BinnedSpringState
+             -> M.Map BinnedSpringState Int
+arrangements ways springState | M.member springState ways = ways
+arrangements ways ([], []) = M.insert ([], []) 1 ways
+arrangements ways ([], groupSizes) = M.insert ([], groupSizes) 0 ways
+arrangements ways (bins, [])
+  | any (any (== '#')) bins = M.insert (bins, []) 0 ways
+  | otherwise = M.insert (bins, []) 1 ways
+arrangements ways (bin:bins, size:groupSizes)
+  | length bin < size
+  = let dropBin | any (== '#') bin = M.insert (bin:bins, size:groupSizes) 0 ways
+                | otherwise
+                , next <- (bins, size:groupSizes)
+                , nWs <- arrangements ways next
+                , Just nrNext <- nWs M.!? next
+                = M.insert (bin:bins, size:groupSizes) nrNext nWs
+     in dropBin
+  | otherwise
+  = let fitWays | (group,bin') <- splitAt size bin
+                , null bin' || head bin' /= '#'
+                , rest <- (dropWhile null (drop 1 bin':bins), groupSizes)
+                , restWays <- arrangements ways rest
+                , Just nrRest <- restWays M.!? rest
+                = M.insert (bin:bins, size:groupSizes) nrRest restWays
+                | otherwise
+                = M.insert (bin:bins, size:groupSizes) 0 ways
+        dropSpring | ('?':bin') <- bin
+                   , next <- (dropWhile null (bin':bins), size:groupSizes)
+                   , nWs <- arrangements fitWays next
+                   , Just nrNext <- nWs M.!? next
+                   = M.insertWith (+) (bin:bins, size:groupSizes) nrNext nWs
+                   | otherwise
+                   = fitWays
+     in dropSpring
+
+nrArrangements :: BinnedSpringState -> Int
+nrArrangements (springs, groupSizes)
+  | binnedSpringState <- (springs, groupSizes)
+  = arrangements M.empty binnedSpringState M.! binnedSpringState
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = sum . map nrArrangements <$> input
+  let answer = sum . map (nrArrangements . first bins) <$> input
   printAnswer "Number of arrangements: " answer
+
+unfoldRecord :: SpringState -> SpringState
+unfoldRecord (springs, groupSizes) = ( intercalate "?" . replicate 5 $ springs
+                                     , mconcat . replicate 5 $ groupSizes
+                                     )
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = sum . map (nrArrangements . first bins . unfoldRecord) <$> input
+  printAnswer "Number of arrangements after unfolding: " answer
 
 main :: IO ()
 main = do
