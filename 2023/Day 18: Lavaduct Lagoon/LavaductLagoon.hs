66839a119798520780e11249cd10a0c7e36b0253
toonn <toonn@toonn.io>
2023-12-18 14:33:48 +0100

    Solve day 18 part 2 in 8 ms
    Solution for part 2 is a much simpler closed formula.
    
    Speeds up part 1 to 5 ms.

--- 2023/Day 18: Lavaduct Lagoon/LavaductLagoon.hs
+++ 2023/Day 18: Lavaduct Lagoon/LavaductLagoon.hs
@@ -7,14 +7,15 @@ import Text.Megaparsec.Char
 
 import AoC
 
-import Data.List (partition)
+import Data.Char (digitToInt)
+import Data.List (elemIndex, partition)
 import qualified Data.Map as M
 import qualified Data.Set as S
 
 type Dir = Char
 type Color = String
 type Instruction = (Dir, Int, Color)
-type Loop = (YX, S.Set YX, M.Map YX Color)
+type Loop = (YX, S.Set YX)
 
 type Input = [Instruction]
 
@@ -32,85 +33,52 @@ instruction = do
 parser :: Parser Input
 parser = sepEndBy1 instruction eol <* eof
 
-digTrench :: YX -> Dir -> Int -> (YX, S.Set YX)
-digTrench (y,x) dir distance =
-  let ((yF,xF),(yT,xT)) | dir == 'U' = ((y - distance, x),(y - 1, x))
-                        | dir == 'R' = ((y, x + 1),(y, x + distance))
-                        | dir == 'D' = ((y + 1, x),(y + distance, x))
-                        | dir == 'L' = ((y, x - distance),(y, x - 1))
-      yx' | dir == 'U' = (yF,xF)
-          | dir == 'R' = (yT,xT)
-          | dir == 'D' = (yT,xT)
-          | dir == 'L' = (yF,xF)
-   in (yx', S.fromAscList [(yy,xx) | yy <- [yF..yT], xx <- [xF..xT]])
-
-dig :: Loop -> Input -> Loop
-dig loop [] = loop
-dig (current, dug, colors) ((dir, distance, color):is) =
-  let (next, trench) = digTrench current dir distance
-      colors' = foldr (\yx more cs -> more (M.insert yx color cs))
-                      id
-                      trench
-                      colors
-   in dig (next, dug <> trench, colors') is
-
-terrain :: S.Set YX -> (YX, YX, S.Set YX)
-terrain loop =
-  let yMin = fst (S.findMin loop)
-      yMax = fst (S.findMax loop)
-      xs = S.map snd loop
-      xMin = S.findMin xs
-      xMax = S.findMax xs
-   in ( (yMin,xMin)
-      , (yMax,xMax)
-      , S.fromAscList [(y,x) | y <- [yMin..yMax], x <- [xMin..xMax]]
-      )
-
-neighbors :: YX -> S.Set YX
-neighbors (y,x) = S.fromAscList [(y + dy, x + dx) | dy <- [-1..1]
-                                                  , dx <- [-1..1]
-                                                  , abs dy /= abs dx
-                                ]
-
-sortIntoBins :: (YX, YX, S.Set YX) -> [S.Set YX]
-sortIntoBins ((yMin,xMin),(yMax,xMax),grid) =
-  foldr (\yx more bins ->
-          let nS = S.filter (\(y,x) ->
-                              yMin <= y && y <= yMax && xMin <= x && x <= xMax
-                            )
-                            (neighbors yx)
-              (otherBins, connectedBins) = partition (S.disjoint nS) bins
-           in more (S.insert yx (mconcat connectedBins) : otherBins)
-        )
-        id
-        grid
-        []
-
-digInterior :: S.Set YX -> S.Set YX
-digInterior loop =
-  let ((yMin,xMin),(yMax,xMax),grid) = terrain loop
-      bins = sortIntoBins ((yMin,xMin), (yMax,xMax), grid S.\\ loop)
-      border = S.fromAscList ( [(yMin,x) | x <- [xMin..xMax]]
-                            <> [(y,x) | y <- [yMin + 1..yMax - 1]
-                                      , x <- [xMin, xMax]
-                               ]
-                            <> [(yMax,x) | x <- [xMin..xMax]]
-                             )
-      interior = mconcat (filter (S.disjoint border) bins)
-   in interior
+dropColor :: Instruction -> (Dir, Int)
+dropColor (dir, distance, _) = (dir, distance)
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = length
-             . (\(_,loop,_) -> loop <> digInterior loop)
-             . dig ((0,0), S.singleton (0,0), mempty)
-           <$> input
+  let answer = area . map dropColor <$> input
   printAnswer "Cubic meters of lava: " answer
+
+extractInstruction :: Instruction -> (Dir, Int)
+extractInstruction (_,_,'#':color) =
+  ("RDLU" !! digitToInt (last color), read ("0x" <> init color))
+
+vertices :: [(Dir, Int)] -> [YX]
+vertices instructions = foldr (\(dir, distance) more (y,x) ->
+                                let (y',x') | dir == 'R' = (y, x + distance)
+                                            | dir == 'D' = (y + distance, x)
+                                            | dir == 'L' = (y, x - distance)
+                                            | dir == 'U' = (y - distance, x)
+                                 in (y,x) : more (y',x')
+                              )
+                              (const [])
+                              instructions
+                              (0,0)
+
+-- | Calculate the area of a simple, i.e. non self-intersecting, polygon
+--
+-- Based on a [formula from Wikipedia](
+-- https://en.wikipedia.org/wiki/Polygon#Simple_polygons)
+-- similar to the Shoelace formula.
+area :: [(Dir, Int)] -> Int
+area instructions =
+  let polygon = vertices instructions
+      trench = 1 + (sum (map snd instructions)) `quot` 2
+   in trench
+    + foldr (\(yI1,xI1) more a (yI,xI) ->
+              more (a + xI * yI1 - xI1 * yI) (yI1, xI1)
+            )
+            (const . (\n -> let (q,0) = n `quotRem` 2 in q) . abs)
+            (tail polygon <> [head polygon])
+            0
+            (head polygon)
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = area . map extractInstruction <$> input
+  printAnswer "Cubic meters of lava with the correct instructions: " answer
 
 main :: IO ()
 main = do
