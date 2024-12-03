41781febbddb26217df77a19cbb069e489399c6e
toonn <toonn@toonn.io>
2024-12-03 13:19:03 +0100

    Solve day 3 part 2 in 5.3 ms
    Generalizing parsing slowed down part 1 to 5.2 ms.

--- 2024/Day 03: Mull It Over/MullItOver.hs
+++ 2024/Day 03: Mull It Over/MullItOver.hs
@@ -10,31 +10,56 @@ import AoC
 
 import Data.Maybe (catMaybes)
 
-type Input = [(Int,Int)]
+data Instruction = Mul Int Int | Do | Dont deriving (Show)
+
+type Input = [Instruction]
 
 nom :: Parser String
-nom = takeWhile1P (Just "Anything but m") (/= 'm')
+nom = takeWhile1P (Just "Anything but m or d") (not . (`elem` "dm"))
 
 mulInst :: Parser (Int,Int)
 mulInst = string "mul" *> between (char '(') (char ')')
                                   ((,) <$> integer <* char ',' <*> integer)
 
+instruction :: Parser Instruction
+instruction = try (uncurry Mul <$> mulInst)
+          <|> try (string "do()" *> pure Do)
+          <|> try (string "don't()" *> pure Dont)
+
 parser :: Parser Input
-parser = catMaybes <$> some ( (try (pure <$> mulInst))
-                          <|> (char 'm' *> pure Nothing)
+parser = catMaybes <$> some ( (try (pure <$> instruction))
+                          <|> (oneOf "dm" *> pure Nothing)
                           <|> (nom *> pure Nothing)
                             )
       <* eof
 
+onlyMul :: [Instruction] -> [Int]
+onlyMul = foldr (\i rs -> case i of
+                            Mul a b -> a * b : rs
+                            _ -> rs
+                )
+                []
+
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = sum . map (uncurry (*)) <$> input
+  let answer = sum . onlyMul <$> input
   printAnswer "Sum of multiplications: " answer
+
+enabledMul :: [Instruction] -> [Int]
+enabledMul is = foldr (\i more enabled -> case i of
+                        Mul a b | enabled -> a * b : more enabled
+                        Do -> more True
+                        Dont -> more False
+                        _ -> more enabled
+                      )
+                      (const [])
+                      is
+                      True
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = sum . enabledMul <$> input
+  printAnswer "Sum of enabled multiplications: " answer
 
 main :: IO ()
 main = do
