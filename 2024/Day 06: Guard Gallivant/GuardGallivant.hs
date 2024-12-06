eb0c3418f2a38bdaf4173541e5b13050198f1093
toonn <toonn@toonn.io>
2024-12-06 11:56:49 +0100

    Solve day 6 part 2 in 7.2 s

--- 2024/Day 06: Guard Gallivant/GuardGallivant.hs
+++ 2024/Day 06: Guard Gallivant/GuardGallivant.hs
@@ -11,7 +11,7 @@ import AoC
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-data Orientation = N | E | S | W deriving (Show)
+data Orientation = N | E | S | W deriving (Show, Eq, Ord)
 
 type Input = [[Char]]
 
@@ -30,32 +30,65 @@ turn E = S
 turn S = W
 turn W = N
 
-patrol' :: M.Map YX Char -> S.Set YX -> YX -> Orientation -> S.Set YX
+patrol' :: M.Map YX Char -> [(YX,Orientation)] -> YX -> Orientation
+        -> [(YX,Orientation)]
 patrol' layout seen p o =
   let next = step p o
       spot = M.findWithDefault ' ' next layout
       res | ' ' <- spot = seen
-          | '#' <- spot = patrol' layout seen p (turn o)
-          | otherwise = patrol' layout (S.insert next seen) next o
+          | '#' <- spot, let o' = turn o = patrol' layout ((p,o') : seen) p o'
+          | otherwise = patrol' layout ((next,o) : seen) next o
    in res
 
 
-patrol :: M.Map YX Char -> S.Set YX
+patrol :: M.Map YX Char -> [(YX,Orientation)]
 patrol layout =
   let start = M.foldrWithKey (\k c rest -> if c == '^' then k else rest)
                              (error "No starting position")
                              layout
-   in patrol' layout (S.singleton start) start N
+   in reverse (patrol' layout [(start,N)] start N)
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = length . patrol . foldYX <$> input
+  let answer = length . S.fromList . map fst . patrol . foldYX <$> input
   printAnswer "Distinct positions: " answer
+
+isCycle :: M.Map YX Char -> S.Set (YX,Orientation) -> YX -> Orientation -> Bool
+isCycle layout seen p o =
+  let next = step p o
+      spot = M.findWithDefault ' ' next layout
+      res | S.member (next,o) seen = True
+          | ' ' <- spot = False
+          | '#' <- spot, let o' = turn o
+          = isCycle layout (S.insert (p,o') seen) p o'
+          | otherwise = isCycle layout (S.insert (next,o) seen) next o
+   in res
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = length
+             . (\layout ->
+                 foldr
+                   (\(p,o) more seen ->
+                     let seen' = S.insert p seen
+                         potential = step p o
+                         potIns | S.notMember potential seen'
+                                , M.findWithDefault ' ' potential layout == '.'
+                                , isCycle (M.insert potential '#' layout)
+                                          mempty
+                                          p
+                                          o
+                                = S.insert potential
+                                | otherwise = id
+                      in potIns (more seen')
+                   )
+                   mempty
+                   (patrol layout)
+                   mempty
+               )
+               . foldYX
+               <$> input
+  printAnswer "Potential obstructions: " answer
 
 main :: IO ()
 main = do
