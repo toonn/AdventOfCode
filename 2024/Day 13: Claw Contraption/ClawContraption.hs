0d5871486117964ba1dfbfbd2f3dc1a0d4a2d0c6
toonn <toonn@toonn.io>
2024-12-13 12:01:19 +0100

    Solve day 13 part 1 in 4.8 ms

---
+++ 2024/Day 13: Claw Contraption/ClawContraption.hs
@@ -0,0 +1,75 @@
+module Main where
+
+import Criterion.Main
+import Criterion.Types
+import System.IO.Silently (silence)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import AoC
+
+import Data.Maybe (mapMaybe)
+
+type ClawMachine = ((Int,Int),(Int,Int),(Int,Int))
+
+type Input = [ClawMachine]
+
+xyPair :: Parser (Int, Int)
+xyPair = do char 'X'
+            oneOf "+="
+            x <- integer
+            string ", Y"
+            oneOf "+="
+            y <- integer
+            pure (x,y)
+
+clawMachine :: Parser ClawMachine
+clawMachine = do string "Button A: "
+                 a <- xyPair
+                 eol
+                 string "Button B: "
+                 b <- xyPair
+                 eol
+                 string "Prize: "
+                 prize <- xyPair
+                 eol
+                 pure (a,b,prize)
+
+parser :: Parser Input
+parser = sepEndBy1 clawMachine eol <* eof
+
+abPresses :: ClawMachine -> Maybe (Int,Int)
+abPresses ((ax,ay),(bx,by),(px,py))
+  = let (b,r1) = (py * ax - ay * px) `quotRem` (by * ax - ay * bx)
+        (a,r2) = (px - b * bx) `quotRem` ax
+        res | r1 == 0, r2 == 0 = Just (a,b)
+            | otherwise = Nothing
+     in res
+
+cost :: (Int, Int) -> Int
+cost (a,b) = 3 * a + b
+
+part1 :: Parsed Input -> IO ()
+part1 input = do
+  let answer = sum . map cost . mapMaybe abPresses <$> input
+  printAnswer "Tokens for all prizes: " answer
+
+part2 :: Parsed Input -> IO ()
+part2 input = do
+  let answer = const 'P' <$> input
+  printAnswer "No answer yet: " answer
+
+main :: IO ()
+main = do
+  let day = "Day 13: Claw Contraption"
+  input <- readInput day parser
+  putStrLn ""
+  part1 input
+  part2 input
+  putStrLn ""
+  defaultMainWith (defaultConfig { resamples = 1 }) [
+      bgroup "AoC"
+        [ bench "Part 1" $ nfIO (silence $ readInput day parser >>= part1)
+        , bench "Part 2" $ nfIO (silence $ readInput day parser >>= part2)
+        ]
+    ]
