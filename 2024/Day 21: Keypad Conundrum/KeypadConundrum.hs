a618acdc108bc413db0f66bf6b9d373bd47b5e8d
toonn <toonn@toonn.io>
2024-12-23 22:23:22 +0100

    Solve day 21 part 2 in 15.3 ms
    Speed up part 1 to 930.1 µs with the memoizing for part 2.

--- 2024/Day 21: Keypad Conundrum/KeypadConundrum.hs
+++ 2024/Day 21: Keypad Conundrum/KeypadConundrum.hs
@@ -8,9 +8,9 @@ import Text.Megaparsec.Char
 
 import AoC
 
-import Data.Tuple (swap)
 import Data.Bool (bool)
-import Data.List (isPrefixOf, minimumBy)
+import Data.List (isPrefixOf)
+import qualified Data.Map as M
 import Data.Maybe (maybe)
 
 type Input = [[Char]]
@@ -35,40 +35,6 @@ select = flip (uncurry bool) . (> 0)
 notPrefixOf :: Eq a => Maybe [a] -> [a] -> Bool
 notPrefixOf = (not .) . (. flip isPrefixOf) . flip (maybe False)
 
-inputs :: Int -> Maybe [(Int, Int)] -> (Int, Int) -> [YX]
-inputs layer forbidden (vert,horz)
-  = let upDown = ((0,1), (1,1))
-        leftRight = ((1,0), (1,2))
-        vs = replicate (abs vert) (select vert upDown)
-        hs = replicate (abs horz) (select horz leftRight)
-        sequences = map (<> pure (0,2)) [vs <> hs, hs <> vs]
-        safe = filter (forbidden `notPrefixOf`) sequences
-        compareOnShortest = curry ( uncurry compare
-                                  . both (shortest (layer - 1))
-                                  )
-     in minimumBy compareOnShortest safe
-
-shortest :: Int -> [YX] -> Int
-shortest layer code
-  | layer == 0 = length code
-  | otherwise
-  = let start | layer == 3 = (3,2)
-              | otherwise = (0,2)
-     in foldr (\goal more from ->
-                let forbidden | fst from == fst start
-                              = Just (replicate (snd from) (1,0))
-                              | (y,0) <- from = Just
-                                              $ if layer == 3
-                                                then replicate (3 - y) (1,1)
-                                                else replicate y (0,1)
-                              | otherwise = Nothing
-                    code' = inputs layer forbidden (goal .-. from)
-                 in shortest (layer - 1) code' + more goal
-              )
-              (const 0)
-              code
-              start
-
 -- 379A
 -- ^A^^<<A>>AvvvA
 -- <A>A<AAv<AA>>^AvAA^Av<AAA>^A
@@ -84,21 +50,79 @@ shortest layer code
 -- <AAv<AA>>^
 -- v<<AA>^AA>
 -- <vA<AA>>^AAvA<^A>AAvA
-complexity :: [Char] -> Int
-complexity code = shortest 3 (map keypadCoord code) * read (init code)
+-- inputs :: Int
+--        -> Int
+--        -> Maybe [(Int, Int)]
+--        -> (Int, Int)
+--        -> M.Map (Int, [YX]) Int
+--        -> ([YX], M.Map (Int, [YX]) Int)
+inputs robots layer forbidden (vert,horz) memo
+  = let upDown = ((0,1), (1,1))
+        leftRight = ((1,0), (1,2))
+        vs = replicate (abs vert) (select vert upDown)
+        hs = replicate (abs horz) (select horz leftRight)
+        sequences = map (<> pure (0,2)) [vs <> hs, hs <> vs]
+        safe = filter (forbidden `notPrefixOf`) sequences
+     in foldr (\code more (l,c,m) -> let (l',m')
+                                           = shortest robots (layer - 1) m code
+                                         (lm,c') | null c || l' < l = (l',code)
+                                                 | otherwise = (l,c)
+                                      in more (lm, c',m')
+              )
+              (\(_,c,m) -> (c,m))
+              safe
+              (0, [], memo)
+
+-- shortest :: Int -> Int -> M.Map (Int, [YX]) Int -> [YX]
+--          -> (Int, M.Map (Int,[YX]) Int)
+shortest robots layer memo code
+  | Just l <- memo M.!? (layer,code) = (l,memo)
+  | layer == 0 = (length code, M.insert (layer,code) (length code) memo)
+  | otherwise
+  = let start | layer == robots = (3,2)
+              | otherwise = (0,2)
+     in foldr (\goal more (from, presses, memo) ->
+                let forbidden | fst from == fst start
+                              = Just (replicate (snd from) (1,0))
+                              | (y,0) <- from = Just
+                                              $ if layer == robots
+                                                then replicate (3 - y) (1,1)
+                                                else replicate y (0,1)
+                              | otherwise = Nothing
+                    (code', memo')
+                      = inputs robots layer forbidden (goal .-. from) memo
+                    (l,memo'') = shortest robots (layer - 1) memo' code'
+                 in more (goal, presses + l, memo'')
+              )
+              (\(_, presses, memo) ->
+                (presses, M.insert (layer,code) presses memo)
+              )
+              code
+              (start, 0, memo)
+
+complexity :: Int -> [Char] -> Int
+complexity robots code
+  = fst (shortest robots robots mempty (map keypadCoord code))
+  * read (init code)
 
 render :: [YX] -> String
 render = map (\(y,x) -> [[ error "The Void!", '^', 'A' ], "<v>" ] !! y !! x)
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = sum . map complexity <$> input
+  let answer = sum . map (complexity 3) <$> input
   printAnswer "Sum of complexities: " answer
 
+-- 123143582477980 too low
+-- v<A<A>>^Av<<A>>^AAvAA^<A>Av<A^>AA<Av<A>>^AvA^Av<A<AA>>^AvAA^<A>Av<A^>A<A>Av<A<AA>>^AvAA^<A>Av<A<A>>^AvA^A<A>Av<<A>>^AvA^Av<A<AA>>^AvA^A<Av>A^AAv<<A>>^Av<A>A^A<A>A
+-- v<A<AA>>^AvAA^<A>Av<<A>>^AvA^Av<<A>>^Av<A>A^A<A>Av<<A>A^>AA<Av>A^A
+-- v<<A>>^A<A>A<Av>A^A<vAA^>A
+-- <A^A^>AvvA
+-- 026A
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = Right 'P' -- const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = sum . map (complexity 26) <$> input
+  printAnswer "Sum with 25 robots: " answer
 
 main :: IO ()
 main = do
--- 2024/Day 21: Keypad Conundrum/README.md
+++ 2024/Day 21: Keypad Conundrum/README.md
@@ -129,12 +129,50 @@ Find the fewest number of button presses you\'ll need to perform in
 order to cause the robot in front of the door to type each code. *What
 is the sum of the complexities of the five codes on your list?*
 
-To begin, [get your puzzle input](21/input).
+Your puzzle answer was `246990`.
 
-Answer:
+## \-\-- Part Two \-\-- {#part2}
+
+Just as the missing Historian is released, The Historians realize that a
+*second* member of their search party has also been missing [this entire
+time]{title="bum bum BUUUUUM"}!
+
+A quick life-form scan reveals the Historian is also trapped in a locked
+area of the ship. Due to a variety of hazards, robots are once again
+dispatched, forming another chain of remote control keypads managing
+robotic-arm-wielding robots.
+
+This time, many more robots are involved. In summary, there are the
+following keypads:
+
+-   One directional keypad that *you* are using.
+-   *25* directional keypads that *robots* are using.
+-   One numeric keypad (on a door) that a *robot* is using.
+
+The keypads form a chain, just like before: your directional keypad
+controls a robot which is typing on a directional keypad which controls
+a robot which is typing on a directional keypad\... and so on, ending
+with the robot which is typing on the numeric keypad.
+
+The door codes are the same this time around; only the number of robots
+and directional keypads has changed.
+
+Find the fewest number of button presses you\'ll need to perform in
+order to cause the robot in front of the door to type each code. *What
+is the sum of the complexities of the five codes on your list?*
+
+Your puzzle answer was `306335137543664`.
+
+Both parts of this puzzle are complete! They provide two gold stars:
+\*\*
+
+At this point, you should [return to your Advent calendar](/2024) and
+try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](21/input).
 
 You can also [\[Share[on
-[Bluesky](https://bsky.app/intent/compose?text=%22Keypad+Conundrum%22+%2D+Day+21+%2D+Advent+of+Code+2024+%23AdventOfCode+https%3A%2F%2Fadventofcode%2Ecom%2F2024%2Fday%2F21)
+[Bluesky](https://bsky.app/intent/compose?text=I%27ve+completed+%22Keypad+Conundrum%22+%2D+Day+21+%2D+Advent+of+Code+2024+%23AdventOfCode+https%3A%2F%2Fadventofcode%2Ecom%2F2024%2Fday%2F21)
 [Twitter](https://twitter.com/)
 [Mastodon](https://mastodon.social/)]{.share-content}\]]{.share} this
 puzzle.
