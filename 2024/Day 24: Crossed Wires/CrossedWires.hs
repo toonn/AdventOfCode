06651d4287f8ca160a7a50aa137dda988c01110d
toonn <toonn@toonn.io>
2024-12-24 19:46:35 +0100

    Solve day 24 part 2 in 2.7 ms
    Had to rely on the structure of a full adder based on AND/OR/XOR and
    knowing the edge cases, the first bit is just an XOR of two inputs, the
    second bit gets a simple AND of two inputs as a carry in and the last
    bit is a carry out.

--- 2024/Day 24: Crossed Wires/CrossedWires.hs
+++ 2024/Day 24: Crossed Wires/CrossedWires.hs
@@ -8,14 +8,19 @@ import Text.Megaparsec.Char
 
 import AoC
 
+import Control.Arrow ((***))
 import Data.Bits ( (.&.)
                  , (.^.)
                  , (.|.)
                  , (.<<.)
                  )
 import Data.Char (isAlphaNum)
+import Data.List (intercalate, sort)
 import Data.Tuple (swap)
 import qualified Data.Map as M
+import qualified Data.Set as S
+
+newtype Swaps = Swaps [String]
 
 type Input = ([(String, Int)], [((String, String, String), String)])
 
@@ -40,6 +45,9 @@ gates = sepEndBy1 ( (,)
 parser :: Parser Input
 parser = (,) <$> initialValues <* eol <*> gates <* eof
 
+arrange :: Input -> (M.Map String Int, M.Map String (String, String, String))
+arrange = M.fromList *** M.fromList . map swap
+
 draw :: M.Map String (String, String, String) -> M.Map String Int -> String
      -> M.Map String Int
 draw gateM stateM wire
@@ -54,32 +62,66 @@ draw gateM stateM wire
                             "XOR" -> (.^.)
                  in M.insert wire (lv `opf` rv) stateM'
 
-decimal :: Input -> Int
-decimal (initials, gates) = let initM = M.fromList initials
-                                gateM = M.fromList . map swap $ gates
-                                zs = filter (\(l:_) -> l == 'z')
-                                   . map fst
-                                   . M.toDescList
-                                   $ gateM
-                             in foldr (\wire more d stateM ->
-                                        let stateM' = draw gateM stateM wire
-                                            d' = d .<<. 1 + stateM' M.! wire
-                                         in more d' stateM'
-                                      )
-                                      const
-                                      zs
-                                      0
-                                      initM
+decimal :: (M.Map String Int, M.Map String (String, String, String)) -> Int
+decimal (initM, gateM) = let zs = filter (\(l:_) -> l == 'z')
+                                . map fst
+                                . M.toDescList
+                                $ gateM
+                          in foldr (\wire more d stateM ->
+                                     let stateM' = draw gateM stateM wire
+                                         d' = d .<<. 1 + stateM' M.! wire
+                                      in more d' stateM'
+                                   )
+                                   const
+                                   zs
+                                   0
+                                   initM
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = decimal <$> input
+  let answer = decimal . arrange <$> input
   printAnswer "Decimal on z wires: " answer
 
+swaps :: M.Map String (String, String, String) -> Swaps
+swaps gateM
+  = Swaps
+  . foldr (\(k, (l,op,r)) ->
+            let ins | ('z':nr) <- k, (nr /= "45" || op /= "OR") && op /= "XOR"
+                    = (k:)
+                    | Just (ll,lop,_) <- gateM M.!? l
+                    , Just (rl,rop,_) <- gateM M.!? r
+                    = let inp | op == "OR" && [lop,rop] /= ["AND","AND"]
+                              = if lop /= "AND"
+                                then (l:) else (r:)
+                              | op == "XOR" && (drop 1 k == "01")
+                             && sort [lop,rop] /= ["AND","XOR"]
+                              = if lop == "XOR" && (drop 1 ll /= "01")
+                                then (l:) else (r:)
+                              | op == "XOR" && not (drop 1 k == "01")
+                             && sort [lop,rop] /= ["OR","XOR"]
+                              = if lop == "XOR" && (drop 1 k /= drop 1 ll)
+                                || lop == "AND"
+                                then (l:) else (r:)
+                              | otherwise = id
+                       in inp
+                    | otherwise = id
+             in ins
+          )
+          mempty
+  . M.assocs
+  $ gateM
+
+instance Show Swaps where
+  show (Swaps swaps) = intercalate "," . sort $ swaps
+
+-- dkj,hkj,hnv,kfm,std,vmv,z21,z35 not right because it's the gates that have
+--   erroneous *inputs*
+-- bqn,cvq,hnv,hth,jnb,tqr,vmv,vwh not the right inputs?
+-- hnv,hth,tqr,vmv,z07,z20,z28,z45 not right z45 is the last *carry*!
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = swaps . snd . arrange <$> input
+  printAnswer "Wires to swap: " answer
 
 main :: IO ()
 main = do
