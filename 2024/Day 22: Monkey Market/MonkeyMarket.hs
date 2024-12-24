a6f0d71334b4ff9e836c6eaff4fcd96d5c26e977
toonn <toonn@toonn.io>
2024-12-24 01:13:48 +0100

    Solve day 22 part 2 in 20.7 s

--- 2024/Day 22: Monkey Market/MonkeyMarket.hs
+++ 2024/Day 22: Monkey Market/MonkeyMarket.hs
@@ -10,6 +10,7 @@ import AoC
 
 import Control.Arrow ((&&&))
 import Data.Bits (xor)
+import qualified Data.Map as M
 
 type Input = [Int]
 
@@ -32,10 +33,31 @@ part1 input = do
   let answer = sum . map (nTimes 2000 step) <$> input
   printAnswer "Sum of 2000th secret numbers: " answer
 
+price :: Int -> Int
+price = (`mod` 10)
+
+fourChangePrices :: [Int] -> [((Int,Int,Int,Int),Int)]
+fourChangePrices ps = let (a:b:c:changes) = zipWith (-) (drop 1 ps) ps
+                          fourChanges = foldr (\d more [a,b,c] ->
+                                                (a,b,c,d) : more [b,c,d]
+                                              )
+                                              (const [])
+                                              changes
+                                              [a,b,c]
+                       in zip fourChanges (drop 4 ps)
+
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = maximum
+             . M.unionsWith (+)
+             . map ( foldMap (uncurry M.singleton)
+                   . fourChangePrices
+                   . map price
+                   . take 2001
+                   . iterate step
+                   )
+           <$> input
+  printAnswer "Most bananas: " answer
 
 main :: IO ()
 main = do
