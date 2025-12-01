32648afa8ddb3bda459eeac59990f515cfeab8da
toonn <toonn@toonn.io>
2025-12-01 18:06:01 +0100

    Solve day 1 part 2 in 21 ms

--- 2025/Day 01: Secret Entrance/SecretEntrance.hs
+++ 2025/Day 01: Secret Entrance/SecretEntrance.hs
@@ -13,29 +13,42 @@ type Input = [(Char,Int)]
 parser :: Parser Input
 parser = sepEndBy1 ((,) <$> lexeme (oneOf "LR") <*> lexeme integer) eol <* eof
 
-zeroes :: Input -> Int
-zeroes instructions = snd
-                    $ foldr (\(dir, steps) next (current, count) ->
-                              let op | dir == 'L' = (-)
-                                     | otherwise  = (+)
-                                  current' = (current `op` steps) `mod` 100
-                                  mInc | current' == 0 = (+ 1)
-                                       | otherwise     = id
-                               in next (current', mInc count)
-                            )
-                            id
-                            instructions
-                            (50,0)
+zeroes :: (Char -> Int -> Int -> Int -> Int) -> Input -> Int
+zeroes increment instructions
+  = snd
+  $ foldr (\(dir, steps) next (current, count) ->
+            let op | dir == 'L' = (-)
+                   | otherwise  = (+)
+                new = (current `op` steps) `mod` 100
+             in next (new, count + increment dir steps current new)
+          )
+          id
+          instructions
+          (50,0)
+
+zeroReaches :: a -> b -> c -> Int -> Int
+zeroReaches _ _ _ new | new == 0  = 1
+                      | otherwise = 0
 
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = zeroes <$> input
+  let answer = zeroes zeroReaches <$> input
   printAnswer "Actual password: " answer
+
+
+zeroPasses :: Char -> Int -> Int -> Int -> Int
+zeroPasses dir steps current new = steps `quot` 100 + passZero
+  where passZero | steps == 0
+                || current == 0
+                || (dir == 'L' && new /= 0 && new < current)
+                || (dir == 'R' && new > current)
+                 = 0
+                 | otherwise = 1
 
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = zeroes zeroPasses <$> input
+  printAnswer "Every zero pass: " answer
 
 main :: IO ()
 main = do
