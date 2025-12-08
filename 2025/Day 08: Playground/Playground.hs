739a2f90c64c30bcba169a3ab91fc779047642da
toonn <toonn@toonn.io>
2025-12-08 16:25:14 +0100

    Solve day 8 part 1 in 5 s

---
+++ 2025/Day 08: Playground/Playground.hs
@@ -0,0 +1,88 @@
+module Main where
+
+import Criterion.Main
+import Criterion.Types
+import System.IO.Silently (silence)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import AoC
+
+import Control.Monad (join)
+import Data.Foldable (foldMap', toList)
+import Data.List (sort, sortBy)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Tuple (swap)
+
+type XYZ = (Int, Int, Int)
+
+type Input = S.Set XYZ
+
+parser :: Parser Input
+parser = S.fromList
+     <$> sepEndBy1 ((,,) <$> integer <* char ','
+                         <*> integer <* char ','
+                         <*> integer
+                   )
+                   eol
+      <* eof
+
+euclidean :: XYZ -> XYZ -> Double
+euclidean (p1,p2,p3) (q1,q2,q3) = sqrt
+                                . fromIntegral
+                                . sum
+                                . map (^ 2)
+                                $ zipWith (-) [p1,p2,p3] [q1,q2,q3]
+
+closestFirst :: S.Set (S.Set XYZ) -> [S.Set XYZ]
+closestFirst = map snd
+             . sort
+             . map swap
+             . M.assocs
+             . M.fromSet (\s -> euclidean (S.findMin s) (S.findMax s))
+             . S.filter ((== 2) . length)
+             . join (foldMap' . flip (S.map . S.union))
+
+connectClosest :: S.Set XYZ
+               -> (S.Set (S.Set XYZ)
+               -> [S.Set (S.Set XYZ)])
+               -> S.Set (S.Set XYZ)
+               -> [S.Set (S.Set XYZ)]
+connectClosest closest next circuits
+  = let (others, close) = S.partition (null . S.intersection closest) circuits
+        connected | length close == 2 = S.insert (S.unions close) others
+                  | otherwise = circuits
+     in connected : next connected
+
+shortestConnections :: Int -> S.Set (S.Set XYZ) -> S.Set (S.Set XYZ)
+shortestConnections n = (!! (n - 1))
+                      . join (foldr connectClosest (const []) . closestFirst)
+
+part1 :: Parsed Input -> IO ()
+part1 input = do
+  let answer = product . take 3 . sortBy (flip compare) . map length . toList
+             . shortestConnections 1000
+             . S.map S.singleton
+           <$> input
+  printAnswer "Size product after 1000 connections: " answer
+
+part2 :: Parsed Input -> IO ()
+part2 input = do
+  let answer = const 'P' <$> input
+  printAnswer "No answer yet: " answer
+
+main :: IO ()
+main = do
+  let day = "Day 08: Playground"
+  input <- readInput day parser
+  putStrLn ""
+  part1 input
+  part2 input
+  putStrLn ""
+  defaultMainWith (defaultConfig { resamples = 1 }) [
+      bgroup "AoC"
+        [ bench "Part 1" $ nfIO (silence $ readInput day parser >>= part1)
+        , bench "Part 2" $ nfIO (silence $ readInput day parser >>= part2)
+        ]
+    ]
