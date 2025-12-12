e581f4f2c56160242ba4dc76cf634243d9e1340d
toonn <toonn@toonn.io>
2025-12-12 22:24:04 +0100

    Solve day 11 part 2 in 14 ms

--- 2025/Day 11: Reactor/README.md
+++ 2025/Day 11: Reactor/README.md
@@ -61,9 +61,64 @@ In total, there are *`5`* different paths leading from `you` to `out`.
 
 *How many different paths lead from `you` to `out`?*
 
-Answer:
+Your puzzle answer was `615`.
 
-To begin, [get your puzzle input](11/input).
+## \-\-- Part Two \-\-- {#part2}
+
+Thanks in part to your analysis, the Elves have figured out a little bit
+about the issue. They now know that the problematic data path passes
+through both `dac` (a [digital-to-analog
+converter](https://en.wikipedia.org/wiki/Digital-to-analog_converter))
+and `fft` (a device which performs a [fast Fourier
+transform](https://en.wikipedia.org/wiki/Fast_Fourier_transform)).
+
+They\'re still not sure which specific path is the problem, and so they
+now need you to find every path from `svr` (the server rack) to `out`.
+However, the paths you find must all also visit both `dac` *and* `fft`
+(in any order).
+
+For example:
+
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out
+
+This new list of devices contains many paths from `svr` to `out`:
+
+    svr,aaa,fft,ccc,ddd,hub,fff,ggg,out
+    svr,aaa,fft,ccc,ddd,hub,fff,hhh,out
+    svr,aaa,fft,ccc,eee,dac,fff,ggg,out
+    svr,aaa,fft,ccc,eee,dac,fff,hhh,out
+    svr,bbb,tty,ccc,ddd,hub,fff,ggg,out
+    svr,bbb,tty,ccc,ddd,hub,fff,hhh,out
+    svr,bbb,tty,ccc,eee,dac,fff,ggg,out
+    svr,bbb,tty,ccc,eee,dac,fff,hhh,out
+
+However, only *`2`* paths from `svr` to `out` visit both `dac` and
+`fft`.
+
+Find all of the paths that lead from `svr` to `out`. *How many of those
+paths visit both `dac` and `fft`?*
+
+Your puzzle answer was `303012373210128`.
+
+Both parts of this puzzle are complete! They provide two gold stars:
+\*\*
+
+At this point, you should [return to your Advent calendar](/2025) and
+try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](11/input).
 
 You can also \[Shareon [Bluesky](https://bsky.app/)
 [Twitter](https://twitter.com/) [Mastodon](https://mastodon.social/)\]
--- 2025/Day 11: Reactor/Reactor.hs
+++ 2025/Day 11: Reactor/Reactor.hs
@@ -10,6 +10,7 @@ import AoC
 
 import Data.Char (isLower)
 import qualified Data.Map as M
+import Data.Monoid (Sum(..))
 import qualified Data.Set as S
 
 type Input = M.Map String [String]
@@ -26,23 +27,42 @@ parser = M.fromList
                    eol
       <* eof
 
-pathCount :: Input -> Int
-pathCount connections = (M.! "out") $ go ["you"] mempty
-  where
-    go :: [String] -> M.Map String Int -> M.Map String Int
-    go [] counts = counts
-    go ds counts = go (foldMap (\d -> M.findWithDefault [] d connections) ds)
-                      (foldl' (\m d -> M.insertWith (+) d 1 m) counts ds)
-
 part1 :: Parsed Input -> IO ()
 part1 input = do
-  let answer = pathCount <$> input
+  let answer = marf "you" "out" <$> input
   printAnswer "Different paths: " answer
 
+partialInsert :: M.Map String (S.Set String) -> String -> [String] -> [String]
+partialInsert _ s [] = [s]
+partialInsert order s (x:xs) | Just True <- elem s <$> order M.!? x = s:x:xs
+                             | otherwise = x : partialInsert order s xs
+
+marf :: String -> String -> Input -> Int
+marf from to connections = getSum . snd
+                         $ go (M.fromList [("out",Sum 0),(to,Sum 1)]) from
+  where
+    go :: M.Map String (Sum Int) -> String -> (M.Map String (Sum Int), Sum Int)
+    go counts f
+      | Just count <- counts M.!? f = (counts,count)
+      | otherwise
+      = (\(cs, c) -> (M.insert f c cs, c))
+      $ foldl' (\(cs, c) n -> (c +) <$> go cs n)
+               (counts, Sum 0)
+               (connections M.! f)
+
+-- Too low: 700666418176
 part2 :: Parsed Input -> IO ()
 part2 input = do
-  let answer = const 'P' <$> input
-  printAnswer "No answer yet: " answer
+  let answer = sum
+             . (\i -> map (\p ->
+               product $ zipWith (\f t -> marf f t i) p (drop 1 p)
+                          )
+                          [ ["svr","fft","dac","out"]
+                          , ["svr","dac","fft","out"]
+                          ]
+               )
+           <$> input
+  printAnswer "Paths through dac and fft: " answer
 
 main :: IO ()
 main = do
